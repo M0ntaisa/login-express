16416b963c7140781ec549b6989370867c9a659c
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-03 14:22:09 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const hbs = require('hbs')
+
+const templatePath = path.join(__dirname,'../templates')
+
+app.use(express.json())
+app.set("view engine", "hbs")
+app.set("views", templatePath)
+
+app.get("/", (req, res) => {
+  res.render("login")
+})
+
+app.get("/regis", (req, res) => {
+  res.render("regis")
+})
 
 app.listen(3000, () => {
   console.log("we're connected to port 3000")
